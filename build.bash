9126127f030a429dfde6995fa20593b2b3592e39
Drew Davis <drew@davisdre.com>
2022-08-08 18:59:11 +0000

    Added build.bash

---
+++ build.bash
@@ -0,0 +1,100 @@
+# Resources
+https://cloudaffaire.com/how-to-create-an-aws-ec2-instance-using-aws-cli/
+https://help.acloud.guru/hc/en-us/articles/360001389256?_ga=2.222229336.1776338761.1659543097-734359405.1648734366
+
+
+# Lets create a key pair
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-key-pair.html#examples
+aws ec2 create-key-pair \
+--key-name MyKeyPair \
+--query 'KeyMaterial' \
+--tag-specifications 'ResourceType=key-pair,Tags=[{Key=Application-Name,Value=MyCloudBuild2},{Key=Resource-Owner,Value=davisdre@hotmail.com}]' \
+--output text > MyKeyPair.pem
+
+
+# Lets create a VPC
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-vpc.html#examples
+aws ec2 create-vpc \
+--cidr-block 10.0.0.0/16 \
+--tag-specifications 'ResourceType=vpc,Tags=[{Key=Application-Name,Value=MyCloudBuild2},{Key=Resource-Owner,Value=davisdre@hotmail.com}]'
+
+
+# Lets create a subnet
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-subnet.html#examples
+aws ec2 create-subnet \
+--vpc-id vpc-03e881f0e63745a53 \
+--cidr-block 10.0.0.0/24 \
+--tag-specifications 'ResourceType=subnet,Tags=[{Key=Application-Name,Value=MyCloudBuild2},{Key=Resource-Owner,Value=davisdre@hotmail.com}]'
+
+# Lets create a internet gateway
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-internet-gateway.html#examples
+aws ec2 create-internet-gateway \
+--tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Application-Name,Value=MyCloudBuild2},{Key=Resource-Owner,Value=davisdre@hotmail.com}]'
+
+# Lets connect our VPC to our Internet gateway
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/attach-internet-gateway.html#examples
+aws ec2 attach-internet-gateway \
+--internet-gateway-id igw-06c2de67941e0c125 \
+--vpc-id vpc-03e881f0e63745a53
+
+# Lets create a route to the Internet Gateway in our route table
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-route.html#examples
+aws ec2 create-route \
+--route-table-id rtb-068a2b7da4d4a107b \
+--destination-cidr-block 0.0.0.0/0 \
+--gateway-id igw-06c2de67941e0c125
+
+# Lets associate our subnet to route table
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/associate-route-table.html#examples
+aws ec2 associate-route-table \
+--route-table-id rtb-068a2b7da4d4a107b \
+--subnet-id subnet-0ade94413dcccde42
+
+# Lets create a security group for our VPC.
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/create-security-group.html#examples
+aws ec2 create-security-group \
+--group-name MyBuild2SecurityGroup \
+--description "MyBuild2 security group" \
+--vpc-id vpc-03e881f0e63745a53
+
+# Lets create some inbound rules in our security group.
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/authorize-security-group-ingress.html#examples
+# Lets open port 22 for SSH. 
+aws ec2 authorize-security-group-ingress \
+--group-id sg-01fb8694664eda51c \
+--protocol tcp \
+--port 22 \
+--cidr 0.0.0.0/0
+
+# Lets open port 80 for http.
+aws ec2 authorize-security-group-ingress \
+--group-id sg-01fb8694664eda51c \
+--protocol tcp \
+--port 80 \
+--cidr 0.0.0.0/0
+
+# Now lets create our EC2 instance
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/run-instances.html#examples
+aws ec2 run-instances \
+--image-id ami-090fa75af13c156b4 \
+--instance-type t2.micro \
+--subnet-id subnet-0ade94413dcccde42 \
+--security-group-ids sg-01fb8694664eda51c \
+--associate-public-ip-address \
+--key-name MyKeyPair \
+--tag-specifications 'ResourceType=instance,Tags=[{Key=Application-Name,Value=MyCloudBuild2},{Key=Resource-Owner,Value=davisdre@hotmail.com}]'
+
+
+# Lets get the public IP of our instance
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instances.html#examples
+aws ec2 describe-instances \
+--instance-ids i-09216cc090c66f4e2 \
+--query "Reservations[*].Instances[*].PublicIpAddress" \
+--output text
+
+# Lets check if the instance is running.
+# https://awscli.amazonaws.com/v2/documentation/api/latest/reference/ec2/describe-instance-status.html#examples
+aws ec2 describe-instance-status \
+--instance-id i-09216cc090c66f4e2
+
+# Lets use the portal to create a AWS CloudShell to SSH into box.
